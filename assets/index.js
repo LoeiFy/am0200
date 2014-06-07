223ffb89b48219efb8b9f20091e11fd9a2340694
LoeiFy <loeify@gmail.com>
2014-06-07 11:03:34 +0800

    add history api

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,8 @@
 
 var section_note = 0,
     pos_hash = true,
+    Dtitle,
+    Durl,
     section_pos;
 
 function moveDown(mark) {
@@ -21,21 +23,33 @@ function moveDown(mark) {
 
     switch (section_note) {
         case 0:
-            section_pos = 0
+            section_pos = 0;
+            Dtitle = 'Lorem Ipsum 2014';
+            Durl = '/';
         break;
 
         case 1:
-            section_pos = $('#gallery').position().top
+            section_pos = $('#gallery').position().top;
+            Dtitle = 'Lorem Ipsum 2014 / Gallery';
+            Durl = '/gallery';
         break;
         
         case 2:
             section_pos = $('#info').position().top
+            Dtitle = 'Lorem Ipsum 2014 / Info';
+            Durl = '/info';
         break;
     } 
 
     API.goTop(section_pos, function() {
+        pushUrl(Dtitle, section_note, Durl)
         setTimeout(function() {pos_hash = true}, 500)
     })
+}
+
+function pushUrl(title, pos, url) {
+    history.pushState({ pos: pos, title: title }, title, url);
+	document.title = title;
 }
 
 $(function() {
@@ -59,5 +73,13 @@ $(function() {
     $(document).keydown(function(e) {
         if (e.keyCode == 40) moveDown(true);
         if (e.keyCode == 38) moveDown(false);
+    })
+
+    window.addEventListener('popstate', function(e) {
+        var state = e.state;
+        if (!state) return;
+        document.title = state.title;
+        section_note = state.pos;
+        API.goTop(section_note * $('#gallery').position().top)
     })
 })
