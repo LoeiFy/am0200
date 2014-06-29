a5642c06182ff1662fabbb2a5ca5ef2ebe87f88e
LoeiFy <loeify@gmail.com>
2014-06-29 10:25:24 +0800

    svg

--- assets/index.js
+++ assets/index.js
@@ -130,6 +130,7 @@ $(function() {
         size()
         appendImg(0, work_0.sum)
         appendImg(1, work_1.sum)
+        draw()
     }, 0)
 
     window.scrollTo(0, 0)
@@ -208,6 +209,33 @@ $(function() {
     })
 
     $('section').css('visibility', 'visible')
+
+    var current_frame = 0;
+    var total_frames = 60;
+    var path = new Array();
+    var length = new Array();
+    for(var i=0; i<2;i++){
+	    path[i] = document.getElementById('i'+i);
+	    l = path[i].getTotalLength();
+	    length[i] = l;
+	    path[i].style.strokeDasharray = l + ' ' + l; 
+	    path[i].style.strokeDashoffset = l;
+    }
+    var handle = 0;
+ 
+ 
+    var draw = function() {
+        var progress = current_frame/total_frames;
+        if (progress > 1) {
+            window.cancelAnimationFrame(handle);
+        } else {
+            current_frame++;
+            for(var j=0; j<path.length;j++){
+	        path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+        }
+        handle = window.requestAnimationFrame(draw);
+        }
+    };
 
     console.log('https://github.com/LoeiFy')
 
