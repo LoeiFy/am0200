f0c540276233d134a09f4447efb98bce3e6df5d2
LoeiFy <loeify@gmail.com>
2015-02-15 22:02:15 +0800

    save

--- lorem.js
+++ lorem.js
@@ -35,6 +35,37 @@ function base64Encode(inputStr) {
 }
 
 /* 
+ * image blur base on canvasBlur
+ */
+function canvasBlur(ele, img) {
+    this.element = ele;
+    this.image = img;
+
+    this.element.width = this.image.width;
+    this.element.height = this.image.height;
+
+    this.context = this.element.getContext('2d');
+    
+    this.context.drawImage(this.image,0,0)
+}
+
+canvasBlur.prototype.blur = function(i) {
+    this.context.globalAlpha = 0.5;
+
+    for (var y = -i; y <= i; y += 2) {
+        for (var x = -i; x <= i; x += 2) {
+            this.context.drawImage(this.element, x + 1, y + 1)
+
+            if (x >= 0 && y >= 0) {
+                this.context.drawImage(this.element, -(x-1), -(y-1))
+            }
+        }
+    }
+
+    this.context.globalAlpha = 1;
+}
+
+/* 
  * get image form localStorage or url
  */
 function getImage(url) {
@@ -45,7 +76,9 @@ function getImage(url) {
     img.onerror = function() {
         loadImage(url)
     }
-    console.log(img.width)
+    var bg = new canvasBlur(document.getElementById('bg'), img)
+    bg.blur(6)
+    //console.log(img.width)
 } 
 
 /* 
