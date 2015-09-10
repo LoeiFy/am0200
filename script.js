c8fdd4a518f9acc2c653765bbb19ff66220e475e
LoeiFy <loeify@gmail.com>
2015-09-10 23:47:21 +0800

    update 20150910

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onload = function() {
     })
 
     console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/LoeiFy")
+    console.log("%c loeify.github.com %c","background:#24272A; color:#ffffff","","https://github.com/LoeiFy/loeify.github.com")
     console.log("%c particles.js %c","background:#24272A; color:#ffffff","","https://github.com/VincentGarreau/particles.js/")
     console.log("%c font %c","background:#24272A; color:#ffffff","","Sansation")
 }
-
