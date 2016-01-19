6e657e18dc8d6efa3c4f23288ff27b0a49964fda
LoeiFy <loeify@gmail.com>
2016-01-19 23:46:15 +0800

    background position

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ var gridWidth = 12,
     row = 31,
     col = 23;
 
-var grid = [[0, 19], [0, 20], [0, 21], [0, 22], [0, 23], [0, 24], [0, 25], [0, 26], [0, 27], [0, 28], [0, 29]];
+var grid = [[0, 19], [0, 20], [0, 4]];
+//var grid = [[0, 19], [0, 20], [0, 21], [0, 22], [0, 23], [0, 24], [0, 25], [0, 26], [0, 27], [0, 28], [0, 29]];
 
 var html = '';
 
@@ -23,16 +24,17 @@ for (var i = 0; i < col; i ++) {
 
     for (var j = 0; j < row; j ++) {
 
-        var mark = '';
+        var mark = '', bg = '';
 
         for (var k = 0; k < grid.length; k ++) {
             if (i == grid[k][0] && j == grid[k][1]) {
                 mark = 'tf';
+                bg = 'background-position:'+ -j*30 +'px '+ -i*30 +'px';
                 break;
             }
         }
 
-        html += '<div style="font-size:10px;float:left;width:30px;height:30px" class="'+ mark +'"></div>'
+        html += '<div style="font-size:10px;float:left;width:30px;height:30px;'+ bg +'" class="'+ mark +'"></div>'
 
     }
 
