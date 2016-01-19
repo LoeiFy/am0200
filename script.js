af98eefabe020bfdadd86b85b4547cf5f779ded8
LoeiFy <loeify@gmail.com>
2016-01-19 22:48:38 +0800

    save

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ window.onload = function() {
     console.log("%c font %c","background:#24272A; color:#ffffff","","Sansation")
 
 }
+
+var gridWidth = 12,
+    row = 31,
+    col = 23;
+
+var grid = [[0, 19], [0, 20], [0, 21], [0, 22], [0, 23], [0, 24], [0, 25], [0, 26], [0, 27], [0, 28], [0, 29]];
+
+var html = '';
+
+for (var i = 0; i < col; i ++) {
+
+    for (var j = 0; j < row; j ++) {
+
+        var mark = '#f9f9f9';
+
+        for (var k = 0; k < grid.length; k ++) {
+            if (i == grid[k][0] && j == grid[k][1]) {
+                mark = 'grey';
+                break;
+            }
+        }
+
+        html += '<div style="font-size:10px;float:left;width:12px;height:12px;background:'+ mark +'"></div>'
+
+    }
+
+}
+
+document.getElementById('test').innerHTML = html;
+document.getElementById('test').style.width = 31 * 12 +'px';
+document.getElementById('test').style.height = 23 * 12 +'px';
+    
