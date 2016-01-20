2762396b83b599ef4fee4bcb7a65c248c68ed36e
LoeiFy <loeify@gmail.com>
2016-01-20 11:48:11 +0800

    interval

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onload = function() {
 
 var gridWidth = 12,
     row = 31,
-    col = 23;
+    col = 21;
 
 var grid = [
     [0, 17], [0, 18], [0, 19], [0, 20], [0, 21], [0, 22], [0, 23], [0, 24], [0, 25],
@@ -36,7 +36,7 @@ var grid = [
     [17, 11], [17, 12], [17, 13], [17, 14], [17, 16], [17, 17],
     [18, 11], [18, 12], [18, 13], [18, 17],
     [19, 11], [19, 17],
-    [20, 11], [20, 12], [20, 17], [20, 18],
+    [20, 11], [20, 12], [20, 17], [20, 18]
 ];
 
 var html = '';
@@ -50,18 +50,56 @@ for (var i = 0; i < col; i ++) {
         for (var k = 0; k < grid.length; k ++) {
             if (i == grid[k][0] && j == grid[k][1]) {
                 mark = 'class="tf"';
-                bg = '<div style="background-position:'+ -j*12 +'px '+ -i*12 +'px"></div>';
+                bg = '<div style="background-position:'+ -j*30 +'px '+ -i*30 +'px"></div>';
                 break;
             }
         }
 
-        html += '<div style="font-size:10px;float:left;width:12px;height:12px;" '+ mark +'>'+ bg +'</div>'
+        html += '<div id="w'+ i +'h'+ j +'" style="font-size:10px;float:left;width:30px;height:30px;" '+ mark +'>'+ bg +'</div>'
 
     }
 
 }
 
 document.getElementById('test').innerHTML = html;
-document.getElementById('test').style.width = 31 * 12 +'px';
-document.getElementById('test').style.height = 23 * 12 +'px';
+document.getElementById('test').style.width = 31 * 30 +'px';
+document.getElementById('test').style.height = 21 * 30 +'px';
+
+
+/*
+document.getElementById('test').addEventListener('mouseover', function(e) {
+    if (e.target.classList.contains('tf')) {
+        e.target.classList.add('active')
+    }
+}, false)
+*/
+
+// shuffle Array
+Array.prototype.shuffle = function() {
+    var i = this.length, p, t;
+    while (i--) {
+        p = Math.floor(Math.random()*i);
+        t = this[i];
+        this[i] = this[p];
+        this[p] = t;
+    }
+    return this
+}
+
+// return shuffle covers
+grid = grid.shuffle();
+
+var start = 0;
+
+t()
+
+function t() {
+setTimeout(function() {
+    document.getElementById('w'+ grid[start][0] +'h'+ grid[start][1]).classList.add('active')
+    if (start < grid.length - 1) {
+        start ++
+        t()
+    }
+}, 100)
+}
     
