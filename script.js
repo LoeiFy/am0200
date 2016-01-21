20fbf86b149a2ca2d16d22559168444def1334a3
LoeiFy <loeify@gmail.com>
2016-01-21 11:09:18 +0800

    feature done

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 
-Array.prototype.shuffle = function() {
-    var i = this.length, p, t;
-    while (i--) {
-        p = Math.floor(Math.random()*i);
-        t = this[i];
-        this[i] = this[p];
-        this[p] = t;
-    }
-    return this
-}
-
-console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/LoeiFy")
-
-window.onload = function() {
-
-    document.getElementsByTagName('h1')[0].classList.add('font')
-
-    Array.prototype.forEach.call(document.getElementsByTagName('a'), function(e, i){
-        e.classList.add('font')
-    })
-
-}
-
 var grid_width = 10,
     row = 31,
     col = 21;
@@ -50,199 +27,80 @@ var grid = [
     [20, 11], [20, 12], [20, 17], [20, 18]
 ];
 
-var html = '';
-
-for (var i = 0; i < col; i ++) {
-
-    for (var j = 0; j < row; j ++) {
-
-        var _id = '', _class = '', _child = '';
-
-        for (var k = 0; k < grid.length; k ++) {
-
-            if (i == grid[k][0] && j == grid[k][1]) {
-                _id = 'id="w'+ i +'h'+ j +'"';
-                _class = 'class="cover"';
-                _child = '<div style="-moz-background-size:'+ row * grid_width +'px '+ col * grid_width +'px;-webkit-background-size:'+ row * grid_width +'px '+ col * grid_width +'px;background-size:'+ row * grid_width +'px '+ col * grid_width +'px;background-position:'+ -j * grid_width +'px '+ -i * grid_width +'px"></div>';
-                break;
-            }
-
-        }
-
-        html += '<div style="width:'+ grid_width +'px;height:'+ grid_width +'px" '+ _id + _class +'>'+ _child +'</div>';
-
+Array.prototype.shuffle = function() {
+    var i = this.length, p, t;
+    while (i--) {
+        p = Math.floor(Math.random()*i);
+        t = this[i];
+        this[i] = this[p];
+        this[p] = t;
     }
-
+    return this
 }
 
-var dinosaur = document.getElementById('dinosaur');
+console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/LoeiFy")
 
-dinosaur.innerHTML = html;
-dinosaur.style.width = row * grid_width +'px';
-dinosaur.style.height = col * grid_width +'px';
+document.addEventListener('DOMContentLoaded', function() {
 
-if (Math.random() > 0.5) {
-    grid = grid.shuffle()
+    var html = '';
+
+    for (var i = 0; i < col; i ++) {
+
+        for (var j = 0; j < row; j ++) {
+
+            var _id = '', _class = '', _child = '';
+
+            for (var k = 0; k < grid.length; k ++) {
+
+                if (i == grid[k][0] && j == grid[k][1]) {
+                    _id = 'id="w'+ i +'h'+ j +'"';
+                    _class = 'class="cover"';
+                    _child = '<div style="-moz-background-size:'+ row * grid_width +'px '+ col * grid_width +'px;-webkit-background-size:'+ row * grid_width +'px '+ col * grid_width +'px;background-size:'+ row * grid_width +'px '+ col * grid_width +'px;background-position:'+ -j * grid_width +'px '+ -i * grid_width +'px"></div>';
+                    break;
+                }
+
+            }
+
+            html += '<div style="width:'+ grid_width +'px;height:'+ grid_width +'px" '+ _id + _class +'>'+ _child +'</div>';
+
+        }
+
+    }
+
+    var dinosaur = document.getElementById('dinosaur');
+
+    dinosaur.innerHTML = html;
+    dinosaur.style.width = row * grid_width +'px';
+    dinosaur.style.height = col * grid_width +'px';
+
+})
+
+window.onload = function() {
+
+    document.getElementsByTagName('h1')[0].classList.add('font')
+
+    Array.prototype.forEach.call(document.getElementsByTagName('a'), function(e, i){
+        e.classList.add('font')
+    })
+
+    document.getElementById('dinosaur').classList.add('loaded')
+
+    if (Math.random() > 0.5) {
+        grid = grid.shuffle()
+    }
+
+    var _start = 0, _time;
+
+    ;(_time = function() {
+
+        setTimeout(function() {
+            document.getElementById('w'+ grid[_start][0] +'h'+ grid[_start][1]).classList.add('active')
+            if (_start < grid.length - 1) {
+                _start ++
+                _time()
+            }
+        }, 100)
+
+    }).call(this)
+
 }
-
-var _start = 0, _time;
-
-;(_time = function() {
-
-    setTimeout(function() {
-        document.getElementById('w'+ grid[_start][0] +'h'+ grid[_start][1]).classList.add('active')
-        if (_start < grid.length - 1) {
-            _start ++
-            _time()
-        }
-    }, 100)
-
-}).call(this)
-
-
-/*
- * https://github.com/LoeiFy/CBFimage/blob/master/README.md
- *
- * @version 1.0.1
- * @author LoeiFy@gmail.com
- * http://lorem.in/ | under MIT license
- */
-
-;(function(window, undefined) {
-
-    function CBFimage(element, option) {
-
-        this.option = {
-            start:      function() {},
-            progress:   function(loaded, total) {},
-            complete:   function(image) {}
-        }
-
-        if (option && typeof option === 'object') {
-            for (var key in option) {
-                this.option[key] = option[key]
-            }
-        }
-
-        if (element && element.getAttribute('src') !== '') {
-            this.element = element;
-
-            this.src = element.getAttribute('src');
-
-            var blur = parseInt(element.getAttribute('blur'));
-            this.blur = blur > 0 && blur <= 10 ? blur : 0;
-
-            this.loadImg()
-        }
-
-    }
-
-    // canvas draw image
-    CBFimage.prototype.canvasImg = function(image) {
-
-        this.element.width = image.width;
-        this.element.height = image.height;
-
-        var context = this.element.getContext('2d');
-        context.drawImage(image, 0, 0)
-
-        if (this.blur > 0) {
-            context.globalAlpha = 0.5;
-
-            for (var y = - this.blur; y <= this.blur; y += 2) {
-                for (var x = - this.blur; x <= this.blur; x += 2) {
-                    context.drawImage(this.element, x + 1, y + 1)
-                    if (x >= 0 && y >= 0) {
-                        context.drawImage(this.element, - (x - 1), - (y - 1))
-                    }
-                }
-            }
-            context.globalAlpha = 1;
-        }
-
-    }
-
-    // xhr get image
-    CBFimage.prototype.loadImg = function () {
-
-        var that = this;
-        this.request = new XMLHttpRequest();
-
-        this.request.onloadstart = function() {
-            that.option.start()
-        }
-
-        this.request.onprogress = function(e) {
-            // may total = 0
-            if (parseInt(e.total) !== 0) {
-                that.option.progress(e.loaded, e.total)
-            }
-        }
-
-        this.request.onload = function(e) {
-            if (this.status >= 200 && this.status < 400) {
-                var image = new Image();
-                image.onload = function() {
-                    that.canvasImg(image)
-                    that.option.complete(image)
-                }
-
-                var type = that.src.substr(that.src.lastIndexOf('.') + 1).substr(0, 3);
-
-                if (type == 'jpg') {
-                    type = 'jpeg'
-                }
-
-                image.src = 'data:image/'+ type +';base64,'+ base64Encode(that.request.responseText);
-            }
-        }
-
-        this.request.open('GET', that.src, true)
-        this.request.overrideMimeType('text/plain; charset=x-user-defined')
-        this.request.send(null)
-
-    }
-
-    // abort
-    CBFimage.prototype.abort = function() {
-        this.request.abort()
-    }
-
-    // http://www.philten.com/us-xmlhttprequest-image/
-    function base64Encode(inputStr) {
-
-        var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-            outputStr = '',
-            i = 0;
-                   
-        while (i < inputStr.length) {
-            //all three "& 0xff" added below are there to fix a known bug 
-            //with bytes returned by xhr.responseText
-            var byte1 = inputStr.charCodeAt(i++) & 0xff,
-                byte2 = inputStr.charCodeAt(i++) & 0xff,
-                byte3 = inputStr.charCodeAt(i++) & 0xff,
-                
-                enc1 = byte1 >> 2,
-                enc2 = ((byte1 & 3) << 4) | (byte2 >> 4),       
-                enc3,
-                enc4;
-
-                if (isNaN(byte2)) {
-                    enc3 = enc4 = 64;
-                } else {
-                    enc3 = ((byte2 & 15) << 2) | (byte3 >> 6);
-                    if (isNaN(byte3)) {
-                        enc4 = 64;
-                    } else {
-                        enc4 = byte3 & 63;
-                    }
-                }
-            outputStr += b64.charAt(enc1) + b64.charAt(enc2) + b64.charAt(enc3) + b64.charAt(enc4);
-        }         
-        return outputStr
-
-    }
-
-    window.CBFimage = CBFimage;
-
-})(window)
