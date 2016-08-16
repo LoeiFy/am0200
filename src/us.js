e507cdba92f6ffa2d77350f943b9572ad4e20220
LoeiFy <loeify@gmail.com>
2016-08-16 16:50:41 +0800

    tiny

--- src/us.js
+++ src/us.js
@@ -1,30 +1,6 @@
 
 var _ = {};
 
-// Establish the root object, `window` (`self`) in the browser, `global`
-// on the server, or `this` in some virtual machines. We use `self`
-// instead of `window` for `WebWorker` support.
-var root = typeof self == 'object' && self.self === self && self ||
-        typeof global == 'object' && global.global === global && global ||
-        this;
-
-var property = function(key) {
-return function(obj) {
-  return obj == null ? void 0 : obj[key];
-};
-};
-
-// Helper for collection methods to determine whether a collection
-// should be iterated as an array or as an object.
-// Related: http://people.mozilla.org/~jorendorff/es6-draft.html#sec-tolength
-// Avoids a very nasty iOS 8 JIT bug on ARM-64. #2094
-var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
-var getLength = property('length');
-var isArrayLike = function(collection) {
-var length = getLength(collection);
-return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
-};
-
 // Similar to ES6's rest param (http://ariya.ofilabs.com/2013/03/es6-and-rest-parameter.html)
 // This accumulates the arguments passed into an array, after a given index.
 var restArgs = function(func, startIndex) {
@@ -91,6 +67,13 @@ return setTimeout(function() {
 }, wait);
 });
 
+// Establish the root object, `window` (`self`) in the browser, `global`
+// on the server, or `this` in some virtual machines. We use `self`
+// instead of `window` for `WebWorker` support.
+var root = typeof self == 'object' && self.self === self && self ||
+        typeof global == 'object' && global.global === global && global ||
+        this;
+
 // Optimize `isFunction` if appropriate. Work around some typeof bugs in old v8,
 // IE 11 (#1621), Safari 8 (#1929), and PhantomJS (#2236).
 var nodelist = root.document && root.document.childNodes;
@@ -108,100 +91,3 @@ if (max == null) {
 }
 return min + Math.floor(Math.random() * (max - min + 1));
 };
-
-// Shuffle a collection.
-_.shuffle = function(obj) {
-return _.sample(obj, Infinity);
-};
-
-// Sample **n** random values from a collection using the modern version of the
-// [Fisher-Yates shuffle](http://en.wikipedia.org/wiki/Fisher–Yates_shuffle).
-// If **n** is not specified, returns a single random element.
-// The internal `guard` argument allows it to work with `map`.
-_.sample = function(obj, n, guard) {
-if (n == null || guard) {
-  if (!isArrayLike(obj)) obj = _.values(obj);
-  return obj[_.random(obj.length - 1)];
-}
-var sample = isArrayLike(obj) ? _.clone(obj) : _.values(obj);
-var length = getLength(sample);
-n = Math.max(Math.min(n, length), 0);
-var last = length - 1;
-for (var index = 0; index < n; index++) {
-  var rand = _.random(index, last);
-  var temp = sample[index];
-  sample[index] = sample[rand];
-  sample[rand] = temp;
-}
-return sample.slice(0, n);
-};
-
-
-// Generate an integer Array containing an arithmetic progression. A port of
-// the native Python `range()` function. See
-// [the Python documentation](http://docs.python.org/library/functions.html#range).
-_.range = function(start, stop, step) {
-if (stop == null) {
-  stop = start || 0;
-  start = 0;
-}
-if (!step) {
-  step = stop < start ? -1 : 1;
-}
-
-var length = Math.max(Math.ceil((stop - start) / step), 0);
-var range = Array(length);
-
-for (var idx = 0; idx < length; idx++, start += step) {
-  range[idx] = start;
-}
-
-return range;
-};
-
-// All **ECMAScript 5** native function implementations that we hope to use
-// are declared here.
-var nativeIsArray = Array.isArray,
-  nativeKeys = Object.keys,
-  nativeCreate = Object.create;
-
-// Is a given value an array?
-// Delegates to ECMA5's native Array.isArray
-_.isArray = nativeIsArray || function(obj) {
-return toString.call(obj) === '[object Array]';
-};
-
-// Is a given variable an object?
-_.isObject = function(obj) {
-var type = typeof obj;
-return type === 'function' || type === 'object' && !!obj;
-};
-
-// An internal function for creating assigner functions.
-var createAssigner = function(keysFunc, defaults) {
-return function(obj) {
-  var length = arguments.length;
-  if (defaults) obj = Object(obj);
-  if (length < 2 || obj == null) return obj;
-  for (var index = 1; index < length; index++) {
-    var source = arguments[index],
-        keys = keysFunc(source),
-        l = keys.length;
-    for (var i = 0; i < l; i++) {
-      var key = keys[i];
-      if (!defaults || obj[key] === void 0) obj[key] = source[key];
-    }
-  }
-  return obj;
-};
-};
-
-// Extend a given object with all the properties in passed-in object(s).
-_.extend = createAssigner(_.allKeys);
-
-// Create a (shallow-cloned) duplicate of an object.
-_.clone = function(obj) {
-if (!_.isObject(obj)) return obj;
-return _.isArray(obj) ? obj.slice() : _.extend({}, obj);
-};
-
