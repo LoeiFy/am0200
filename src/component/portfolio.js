0febddfa9806a39b7908ebfdb22594a190f2dccc
LoeiFy <LoeiFy@gmail.com>
2019-04-08 22:17:58 +0800

    append page

--- src/component/portfolio.js
+++ src/component/portfolio.js
@@ -1,39 +1,10 @@
-import detail from './template/detail.js'
-import $ from './query.js'
+import $ from './query'
 
 export default class {
   constructor(e) {
     this.container = $(e)
   }
 
-  setImgs(imgs) {
-    const { elements } = $(imgs)
-    for (let i = 0; i < elements.length; i += 1) {
-      elements[i].onload = function load() {
-        const { width, height } = this
-        $(this).css({
-          display: 'inline-block',
-          width: `${width * 0.5}px`,
-          height: `${height * 0.5}px`,
-        })
-      }
-    }
-    return this
-  }
-
-  render(name, callback) {
-    this.container.addClass('loading')
-
-    window.fetch(`/json/${name}.json`)
-      .then(res => res.json())
-      .then(res => detail(res))
-      .then((res) => {
-        setTimeout(() => {
-          this.container.removeClass('loading')
-          this.container.html(res)
-          this.setImgs('.detail img')
-          callback()
-        }, 500)
-      })
+  render() {
   }
 }
--- src/index.js
+++ src/index.js
@@ -19,5 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(path)
   }
 
+  portfolios.link = (name) => {
+    window.fetch(`${window.host}/${name}/`)
+      .then(res => res.text())
+      .then((res) => {
+        const main = res.match(/<body>([\s\S]*)<\/body>/)
+        const fragment = document.createElement('div');
+        [fragment.innerHTML] = main
+
+        const child = fragment.querySelector('#main-content > div')
+        $('#main-content').html('').append(child)
+      })
+  }
+
   $('#menu').on('click', () => $('.left').addClass('active'))
 })
