2dc098048a66e2589bd5e01cf55ff06095fa0b36
LoeiFy <LoeiFy@gmail.com>
2019-05-10 21:36:19 +0800

    chore: falco 1.3.1

--- falco.config.js
+++ falco.config.js
@@ -1,10 +1,7 @@
 const { join } = require('path')
 
 module.exports = {
-  entry: {
-    main: 'index.js',
-    path: join(__dirname, 'templates/am0200/source/script'),
-  },
+  entry: join(__dirname, 'templates/am0200/source/script/index.js'),
   sourceMap: false,
   output: {
     filename: 'index.[hash:8].js',
